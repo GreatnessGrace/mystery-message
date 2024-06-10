e82412739459208d3f7c9ac97789df56c4b0423d
GreatnessGrace <graceunstoppable7@gmail.com>
2024-06-10 12:21:27 +0530

    [Completed] UserModel

---
+++ src/app/models/User.ts
@@ -0,0 +1,69 @@
+import mongoose, {Schema, Document} from "mongoose";
+
+    export interface Message extends Document{
+    content: string;  // in js or ts string datatype starts with lowercase 's' 
+    createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+    content: {
+        type: String, //In mongoose string datatype starts with capital'S'
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now,
+    },
+})
+
+export interface User extends Document{
+    username: string;  // in js or ts string datatype starts with lowercase 's' 
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    isAcceptingMessage: boolean;
+    isVerified: boolean,
+    messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+    username: {
+        type: String, //In mongoose string datatype starts with capital'S'
+        required: [true, "Username is required"],
+        trim: true,
+        unique: true,
+    },
+    email: {
+        type: String, 
+        required: [true, "Email is required"],
+        unique: true,
+        match: [/.+\@.+/, 'Please provide valid Email']
+    },
+    password: {
+        type: String, 
+        required: [true, "Password is required"],
+    },
+    verifyCode: {
+        type: String, 
+        required: [true, "Verify code is required"]
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        required: [true, "Verify code Expiry is required"]
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    isAcceptingMessage:{
+        type: Boolean, 
+        default: true
+    },
+    messages: [MessageSchema]
+})
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || (mongoose.model<User>("User", UserSchema));
+
+export default UserModel;
